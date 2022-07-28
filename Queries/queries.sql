26675dd0b82edeefdd5726856c5496c12e8c0a8e
HackUnit <j.larose26@gmail.com>
2022-07-28 15:52:15 -0500

    Updating query files, added csv and moved queries into Queries/

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -134,3 +134,140 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 
 SELECT * FROM retirement_info
+
+
+
+DROP TABLE retirement_info;
+
+
+
+-- Create new table for retiring employees
+
+SELECT emp_no, first_name, last_name
+
+INTO retirement_info
+
+FROM employees
+
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
+-- Check the table
+
+SELECT * FROM retirement_info;
+
+-- Joining departments and managers tables
+
+SELECT departments.dept_name,
+
+	managers.emp_no,
+	
+	managers.from_date,
+	
+	managers.to_date
+	
+FROM departments
+
+INNER JOIN managers
+
+ON departments.dept_no = managers.dept_no;
+
+
+
+-- Joining retirement_info and dept_emp tables
+
+SELECT retirement_info.emp_no,
+
+	retirement_info.first_name,
+	
+retirement_info.last_name,
+
+	dept_emp.to_date
+	
+FROM retirement_info
+
+LEFT JOIN dept_emp
+
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
+
+-- Making previous query shorter with aliases
+
+SELECT ri.emp_no,
+
+	ri.first_name,
+	
+ri.last_name,
+
+	de.to_date
+	
+FROM retirement_info as ri
+
+LEFT JOIN dept_emp as de
+
+ON ri.emp_no = de.emp_no;
+
+
+
+-- Joining departments and managers tables with aliases instead
+
+SELECT d.dept_name,
+
+	ma.emp_no,
+	
+	ma.from_date,
+	
+	ma.to_date
+	
+FROM departments as d
+
+INNER JOIN managers as ma
+
+ON d.dept_no = ma.dept_no;
+
+
+
+-- Query to join retirement_info and dept_emp into current_emp filtered to only current employees
+
+SELECT ri.emp_no,
+
+	ri.first_name,
+	
+	ri.last_name,
+	
+de.to_date
+
+INTO current_emp
+
+FROM retirement_info as ri
+
+LEFT JOIN dept_emp as de
+
+ON ri.emp_no = de.emp_no
+
+WHERE de.to_date = ('9999-01-01');
+
+
+
+-- Employee count by department number
+
+SELECT COUNT(ce.emp_no), de.dept_no
+
+INTO employee_count
+
+FROM current_emp as ce
+
+LEFT JOIN dept_emp as de
+
+ON ce.emp_no = de.emp_no
+
+GROUP BY de.dept_no
+
+ORDER BY de.dept_no;
+
+
+
